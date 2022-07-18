31a373777cd914a0b80d66e9aad674b9c3a5233e
Saeedovic <Seedxlss@gmail.com>
2022-07-19 02:55:56 +0400

    Quest State Machine progress 1

--- Assets/_Scripts/Quests/Quest.cs
+++ Assets/_Scripts/Quests/Quest.cs
@@ -1,21 +1,175 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
+
 
 public class Quest : MonoBehaviour
 {
-    public List<Goal> Goals { get; set; } = new List<Goal>();
+    public FirstMissionState firstMissionState;
+    public SecondMissionState secondMissionState;
+    public ThirdMissionState thirdMissionState;
+    public FourthMissionState fourthMissionState;
+    public FifthMissionState fifthMissionState;
+    public SixthMissionState sixthMissionState;
+    public SeventhMissionState seventhMissionState;
+    QuestState currentState;
 
-    public string Questname;
-    public string Description;
-
-    public bool IsDone;
-
-    public void CheckGoals()
+    void Start()
     {
-        IsDone = Goals.All(g => g.IsDone);
+        
+
+        firstMissionState = new FirstMissionState();
+        secondMissionState = new SecondMissionState();
+        thirdMissionState = new ThirdMissionState();
+        fourthMissionState = new FourthMissionState();
+        fifthMissionState = new FifthMissionState();
+        sixthMissionState = new SixthMissionState();
+        seventhMissionState = new SeventhMissionState();
+
+         currentState = firstMissionState;
+    }
+
+
+    void Update()
+    {
+
+        
+        currentState.UpdateState(this);
+
 
 
     }
+
+
+    public void ChangeState(QuestState desiredState)
+    {
+        currentState = desiredState;
+    }
+    
+    
 }
+
+
+public abstract class QuestState
+{
+
+
+    public abstract void UpdateState(Quest quest);
+
+}
+
+public class FirstMissionState : QuestState
+{
+
+    public override void UpdateState(Quest quest)
+    {
+
+        quest.ChangeState(Quest.SecondMissionState);
+ 
+    }
+
+    
+
+}
+
+
+
+public class SecondMissionState : QuestState
+{
+    
+
+
+    public override void UpdateState(Quest quest)
+    {
+
+
+        quest.ChangeState(Quest.ThirdMissionState);
+
+
+    }
+
+ }
+
+
+public class ThirdMissionState : QuestState
+{
+
+
+
+    public override void UpdateState(Quest quest)
+    {
+
+
+        quest.ChangeState(Quest.FourthMissionState);
+
+
+    }
+
+}
+
+public class FourthMissionState : QuestState
+{
+
+
+
+    public override void UpdateState(Quest quest)
+    {
+
+
+        quest.ChangeState(Quest.FifthMissionState);
+
+
+    }
+
+}
+
+public class FifthMissionState : QuestState
+{
+
+
+
+    public override void UpdateState(Quest quest)
+    {
+
+
+        quest.ChangeState(Quest.SixthMissionState);
+
+
+    }
+
+}
+
+public class SixthMissionState : QuestState
+{
+
+
+
+    public override void UpdateState(Quest quest)
+    {
+
+
+        quest.ChangeState(Quest.SeventhMissionState);
+
+
+    }
+
+}
+
+public class SeventhMissionState : QuestState
+{
+
+
+
+    public override void UpdateState(Quest quest)
+    {
+
+
+        quest.ChangeState(Quest.MissionState);
+
+
+    }
+
+}
+
+
+
